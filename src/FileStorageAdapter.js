1e68de40cd6538f85ddab9bf2296eb3372f81268
Andreas Rozek <A.Rozek@gmx.de>
2023-05-24 11:03:17 +0200

    Update FileStorageAdapter.js

--- src/FileStorageAdapter.js
+++ src/FileStorageAdapter.js
@@ -1,4 +1,4 @@
-/**** simple file storage adapter ****/
+/**** simple filtering file storage adapter ****/
 
   const fs   = require('fs')
   const path = require('path')
@@ -6,25 +6,86 @@
   GUN.on('create', function (Context) {        // do not use a "fat arrow" here!
     this.to.next(Context)
 
-    let StorageFile = Context.opt.simpleFileStorage
-    if (StorageFile == null) { return }              // adapter wasn't requested
+    let   StorageFile = undefined
+    const Filters     = []
+    let   KeyHole     = {}
+
+    let Options = Context.opt.simpleFileStorage
+    switch (true) {
+      case (Options ==  null):
+      case (Options === false):
+        return                                       // adapter wasn't requested
+      case (typeof Options === 'string'):
+        StorageFile = Options
+        break
+      case Array.isArray(Options):
+        throw new TypeError('FileStorageAdapter: invalid options given')
+      case (typeof Options === 'object'):
+        if (typeof Options.file === 'string') {
+          StorageFile = Options.file
+        } else {
+          throw new TypeError('FileStorageAdapter: missing or invalid storage file path')
+        }
+
+        switch (true) {
+          case (Options.filter == null):
+            break
+          case (typeof Options.filter === 'string'):
+            Filters.push(Options.filter)
+            break
+          case Array.isArray(Options.filter):
+            Options.filter.forEach((Filter) => {
+              if (typeof Filter === 'string') {
+                Filters.push(Filter)
+              } else {
+                throw new TypeError('FileStorageAdapter: invalid filter given')
+              }
+            })
+            break
+          default:
+            throw new TypeError('FileStorageAdapter: invalid filter(s) given')
+        }
+
+        if (typeof Options.keyhole === 'object') {
+          KeyHole = Options.keyhole || {}
+        }
+        break
+      default:
+        throw new TypeError('FileStorageAdapter: invalid options given')
+    }
+
+    function IdIsPermitted (Id) {
+      if (Filters.length === 0) { return true }
+        for (let i = 0, l = Filters.length; i < l; i++) {
+          const Filter = Filters[i]
+          if (Filter.endsWith('/')) {
+            if (Id.startsWith(Filter)) { return true }
+          } else {
+            if (
+              (Id === Filter) || Id.startsWith(Filter + '/')
+            ) { return true }
+          }
+        }
+      return false
+    }
 
     StorageFile = path.join(__dirname,StorageFile)   // make file spec. absolute
 
   /**** to speed persistence up: keep a copy of the full store in memory ****/
 
-    let Storage
+    let Storage = KeyHole.Storage = {}
       let Stats = fs.statSync(StorageFile, {throwIfNoEntry:false}) // may crash!
     if (Stats == null) {
-      Storage = {}
       fs.writeFileSync(StorageFile,JSON.stringify(Storage))        // may crash!
     } else {
       if (! Stats.isFile) {
         throw new Error('the given storage "file" is actually not a file')
       }
 
-      Storage = JSON.parse(fs.readFileSync(StorageFile))           // may crash!
+      Storage = KeyHole.Storage = JSON.parse(fs.readFileSync(StorageFile)) // may crash!
     }
+
+    KeyHole.PutCount = 0
 
   /**** get - retrieve the contents of a given node ****/
 
@@ -36,7 +97,12 @@
       let Soul    = (LEX == null ? undefined : LEX['#'])
       if (Soul == null) { return }
 
-      let Data = Storage[Soul]                      // fetches data from storage
+      if (! IdIsPermitted(Soul)) {                                // "fail fast"
+        Context.on('in', { '@':DedupId, err:null, put:null })
+        return
+      }
+
+      let Data = KeyHole.Storage[Soul]              // fetches data from storage
       if (Data == null) {
         Context.on('in', { '@':DedupId, err:null, put:null })
       } else {
@@ -60,6 +126,14 @@
       let Soul    = LEX['#'], Key  = LEX['.']
       let DedupId = WireMessage['#']
       let Ok      = WireMessage.ok || ''
+
+      if (! IdIsPermitted(Soul)) {                                // "fail fast"
+//      Context.on('in', { '@':DedupId, err:'undesired node id', ok:false })
+        Context.on('in', { '@':DedupId, err:null, ok:false })
+        return
+      }
+
+      KeyHole.PutCount++
 
       Storage[Soul] = GUN.state.ify(             // merges new data into storage
         Storage[Soul], Key, LEX['>'], LEX[':'], Soul
